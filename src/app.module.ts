27f9860b5b633841d5129a3ae3cdb3821eb1f337
Brandon-Online01 <Brandonnkawu01@gmail.com>
2024-11-26 11:39:59 +0200

    ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { Auth } from './auth/entities/auth.entity';
-import { EmployeeProfile } from './user/entities/user.entity';
-import { User } from './user/entities/user.entity';
-import { EmploymentProfile } from './user/entities/user.entity';
+
 @Module({
   imports: [
     EventEmitterModule.forRoot(),
@@ -19,13 +14,13 @@ import { EmploymentProfile } from './user/entities/user.entity';
     }),
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [Auth, User, EmployeeProfile, EmploymentProfile],
+      entities: [],
       synchronize: true,
       retryAttempts: 50,
       retryDelay: 5000,
@@ -33,8 +28,6 @@ import { EmploymentProfile } from './user/entities/user.entity';
         connectionLimit: 1000,
       },
     }),
-    AuthModule,
-    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
